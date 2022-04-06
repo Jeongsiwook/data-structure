6d354d30c7ccc959643f18dd7fc5fd3fe68e7c67
siwook <jsw0638@gmail.com>
2022-04-06 23:48:01 +0900

    Update heap.py

--- Tree,Priority-queue,Heap/heap.py
+++ Tree,Priority-queue,Heap/heap.py
@@ -1,3 +1,4 @@
+# 최소 힙
 import heapq
 
 class PriorityQueue:
@@ -16,3 +17,24 @@ class PriorityQueue:
             return -1
         else:
             return self.data[0]
+
+        
+# 절대값 힙
+import heapq
+
+class PriorityQueue:
+    def __init__(self) :
+        self.data = []
+
+    def push(self, value) :
+        heapq.heappush(self.data, (abs(value), value))
+                
+    def pop(self) :
+        if len(self.data) > 0:
+            heapq.heappop(self.data)
+        
+    def top(self) :        
+        if len(self.data) == 0:
+            return -1
+        else:
+            return self.data[0][1]
